e65fd8696c2371f3d357792ba654aab0646a4f27
rishabhpreethan <rishabh_preethan@thirdray.ai>
2025-07-03 12:04:34 +0530

    Disable ESLint and TypeScript checks during build

--- next.config.js
+++ next.config.js
@@ -6,7 +6,18 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Optional: Add any other Next.js config options here
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
